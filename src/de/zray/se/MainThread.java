70e5daf2793f087e02f98b6d78f14c68c95c3ff8
vortex <vortex@192.168.178.123>
2017-05-19 15:30:43 +0200

    Try to fix LWJGL3 Setup

--- src/de/zray/se/MainThread.java
+++ src/de/zray/se/MainThread.java
@@ -5,17 +5,20 @@
  */
 package de.zray.se;
 
-import de.zray.se.commands.Mute;
-import de.zray.se.logger.SELogger;
-import de.zray.se.script.Console;
-import de.zray.se.script.exceptions.DublicateCommandException;
 import java.io.IOException;
-import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.ContextAttribs;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-import static org.lwjgl.opengl.GL11.glClearColor;
-import org.lwjgl.opengl.PixelFormat;
+
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 /**
  *
@@ -24,60 +27,106 @@ import org.lwjgl.opengl.PixelFormat;
 public class MainThread {
     private static boolean closeRequested = false;
     private static SEWorld currentWorld;
-    private static Console console;
     
-    public static void loop(SEWorld world) throws IOException{
-        currentWorld = world;
-        if(console == null){
-            createConsole();
-        }
-        if(!Display.isCreated()){
-            createDisplay();
-        }
-        
-        while(!closeRequested){
-            world.act();
-            Display.update();
+    private long window;
+    
+    public void run(SEWorld world) throws IOException {
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
+        init();
+        loop(world);
+
+        // Free the window callbacks and destroy the window
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+
+        // Terminate GLFW and free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+    
+    private void loop(SEWorld world) throws IOException{
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+
+        // Set the clear color
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+
+        // Run the rendering loop until the user has attempted to close
+        // the window or has pressed the ESCAPE key.
+        while ( !glfwWindowShouldClose(window) ) {
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+                glfwSwapBuffers(window); // swap the color buffers
+
+                // Poll for window events. The key callback above will only be
+                // invoked during this call.
+                glfwPollEvents();
         }
     }
     
-    public static SEWorld getCurrentWorld(){
+    public SEWorld getCurrentWorld(){
         return currentWorld;
     }
     
-    public static Console getConsole(){
-        if(console == null){
-            createConsole();
-        }
-        return console;
-    }
-    
-    public static void shutdown(){
+    public void shutdown(){
         System.exit(0);
     }
     
-    private static void createConsole(){
-        try {
-            console = new Console();
-            console.addCommand(new Mute());
-        } catch (DublicateCommandException ex) {
-            SELogger.get().dispatchMsg("MainThread", SELogger.SELogType.ERROR, new String[]{ex.getMessage()}, false);
-        }
-    }
-    
-    private static void createDisplay(){
-        PixelFormat pixelFormat = new PixelFormat();
-        ContextAttribs contextAtrributes = new ContextAttribs(3, 0);
-        
-        try {
-                Display.setDisplayMode(new DisplayMode(Settings.get().window.resX, Settings.get().window.resY));
-                Display.setTitle(Settings.get().title+" "+Settings.get().version);
-                Display.setResizable(Settings.get().window.displayRezisable);
-                Display.create(pixelFormat, contextAtrributes);
-        } catch (LWJGLException e) {
-                e.printStackTrace();
-                System.exit(-1);
-        }
-        glClearColor(0.1f, 0.1f, 0.1f, 0f);
+    private void init(){
+        // Setup an error callback. The default implementation
+        // will print the error message in System.err.
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if ( !glfwInit() )
+                throw new IllegalStateException("Unable to initialize GLFW");
+
+        // Configure GLFW
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+
+        // Create the window
+        window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+        if ( window == NULL )
+                throw new RuntimeException("Failed to create the GLFW window");
+
+        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+                if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+                        glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+        });
+
+        // Get the thread stack and push a new frame
+        try ( MemoryStack stack = stackPush() ) {
+                IntBuffer pWidth = stack.mallocInt(1); // int*
+                IntBuffer pHeight = stack.mallocInt(1); // int*
+
+                // Get the window size passed to glfwCreateWindow
+                glfwGetWindowSize(window, pWidth, pHeight);
+
+                // Get the resolution of the primary monitor
+                GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+                // Center the window
+                glfwSetWindowPos(
+                        window,
+                        (vidmode.width() - pWidth.get(0)) / 2,
+                        (vidmode.height() - pHeight.get(0)) / 2
+                );
+        } // the stack frame is popped automatically
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window);
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(window);
     }
 }
