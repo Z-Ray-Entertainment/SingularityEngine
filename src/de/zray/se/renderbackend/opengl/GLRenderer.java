3a136b305a92a26d1b6c1e6470326295abaa4b11
Vortex Acherontic <vortex@z-ray.de>
2017-07-10 01:30:23 +0200

    changed GLRenderer pipeline

--- src/de/zray/se/renderbackend/opengl/GLRenderer.java
+++ src/de/zray/se/renderbackend/opengl/GLRenderer.java
@@ -9,6 +9,7 @@ import de.zray.se.SEActor;
 import de.zray.se.SEWorld;
 import de.zray.se.Settings;
 import de.zray.se.grapics.Camera;
+import de.zray.se.grapics.semesh.SEMaterial;
 import de.zray.se.grapics.semesh.SEMesh;
 import de.zray.se.logger.SELogger;
 import de.zray.se.renderbackend.RenderBackend;
@@ -38,6 +39,7 @@ public class GLRenderer implements RenderBackend{
     private int windowH = Settings.get().window.resY;
     private boolean closeRequested = false;
     private List<OpenGLRenderData> oglRenderDatas = new LinkedList<>();
+    GLUtils glUtils = new GLUtils();
 
     @Override
     public boolean init() {
@@ -99,10 +101,9 @@ public class GLRenderer implements RenderBackend{
         applyCamera(world.getCurrentCamera());
         for(SEActor actor : world.getActors()){
             for(SEMesh mesh : actor.getRendableSEMeshes()){
-                if(mesh.getRenderData() == -1){
-                    OpenGLRenderData rData = new OpenGLRenderData();
-                }
-                switch(mesh.getRe){}
+                glPushMatrix();
+                renderMesh(mesh);
+                glPopMatrix();
             }
             
         }        
@@ -182,6 +183,61 @@ public class GLRenderer implements RenderBackend{
         return windowH;
     }
     
-    private void renderSEMesh(SEMesh mesh){}
+    private void renderMesh(SEMesh mesh){
+        double posX = mesh.getOrientation().getPositionVec().x;
+        double posY = mesh.getOrientation().getPositionVec().y;
+        double posZ = mesh.getOrientation().getPositionVec().z;
+        double rotX = mesh.getOrientation().getRotationVec().x;
+        double rotY = mesh.getOrientation().getRotationVec().y;
+        double rotZ = mesh.getOrientation().getRotationVec().z;
+        double scaleX = mesh.getOrientation().getScaleVec().x;
+        double scaleY = mesh.getOrientation().getScaleVec().y;
+        double scaleZ = mesh.getOrientation().getScaleVec().z;
+        
+        glTranslated(posX, posY, posZ);
+        glRotated(rotX, 1, 0, 0);
+        glRotated(rotY, 0, 1, 0);
+        glRotated(rotZ, 0, 0, 1);
+        glScaled(scaleX, scaleY, scaleZ);
+        
+        if(mesh.getRenderData() == -1){
+            OpenGLRenderData rData = new OpenGLRenderData();
+            switch(mesh.getRenderMode()){
+                case DISPLAY_LIST :
+                    glUtils.generateDisplayList(mesh, rData);
+                    glUtils.generateDisplayListWired(mesh, rData);
+                    break;
+                case VBO :
+                    glUtils.generateVBO(mesh, rData);
+                    glUtils.generateVBO(mesh, rData);
+                    break;
+            }
+
+        }
+        
+        applyMaterial(mesh.getMaterial());
+        
+        switch(mesh.getRenderMode()){
+            case DIRECT :
+                glUtils.drawObject(mesh);
+                break;
+            case DISPLAY_LIST :
+                int listID = oglRenderDatas.get(mesh.getRenderData()).getDisplayList();
+                glCallList(listID);
+                break;
+            case VBO :
+                glUtils.renderVBO(oglRenderDatas.get(mesh.getRenderData()));
+                break;
+        }
+    }
+    
+    private void applyMaterial(SEMaterial mat){
+        if(mat.cullBackfaces() && !glIsEnabled(GL_CULL_FACE)){
+            glEnable(GL_CULL_FACE);
+        }
+        else if(glIsEnabled(GL_CULL_FACE)){
+            glDisable(GL_CULL_FACE);
+        }
+    }
 
 }
