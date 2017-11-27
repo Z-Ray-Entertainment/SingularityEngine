4daf3550b401595be532edd549d20e0bfbe5a383
Vortex Acherontic <vortex@z-ray.de>
2017-11-27 18:20:05 +0100

    Moved SEUtils to utils Package

--- src/de/zray/se/utils/SEUtils.java
+++ src/de/zray/se/utils/SEUtils.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.zray.se;
+package de.zray.se.utils;
 
 import java.util.Random;
 import javax.vecmath.Vector2d;
