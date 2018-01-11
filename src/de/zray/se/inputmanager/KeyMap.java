a66d8edda32257af85af356c01e7a10f5fdf1754
Vortex Acherontic <vortex@z-ray.de>
2018-01-11 16:18:33 +0100

    Adding AROWW_RIGHT to KeyMap (w.i.p)

--- src/de/zray/se/inputmanager/KeyMap.java
+++ src/de/zray/se/inputmanager/KeyMap.java
@@ -17,7 +17,7 @@ public class KeyMap {
             KEY_L = 76, KEY_M = 77, KEY_N = 78, KEY_O = 79, KEY_P = 80, KEY_Q = 81,
             KEY_R = 82, KEY_S = 83, KEY_T = 84, KEY_U = 85, KEY_V = 86, KEY_W = 87,
             KEY_X = 88, KEY_Y = 89, KEY_Z = 90, KEY_ARROW_UP = 265, KEY_ARROW_DOWN = 264,
-            KEY_ARROW_LEFT = 263, KEY_ESCAPE = 256, KEY_PAGE_UP = 266, KEY_PAGE_DOWN = 267,
+            KEY_ARROW_LEFT = 263, KEY_ARROW_RIGHT = 0, KEY_ESCAPE = 256, KEY_PAGE_UP = 266, KEY_PAGE_DOWN = 267,
             KEY_LEFT_SHIFT = 340;
     
     public KeyMap(KeyBoardLayout layout){
