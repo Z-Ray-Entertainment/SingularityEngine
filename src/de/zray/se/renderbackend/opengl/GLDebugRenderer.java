3cfbc43090523642ecb85a2e1447e915f198ea01
Vortex Acherontic <vortex@z-ray.de>
2018-04-17 22:22:08 +0200

    #15 fixed some dps not beeing rendered in glDebugRenderer

--- src/de/zray/se/renderbackend/opengl/GLDebugRenderer.java
+++ src/de/zray/se/renderbackend/opengl/GLDebugRenderer.java
@@ -58,39 +58,57 @@ public class GLDebugRenderer {
     private void renderDistancePatches(World world){
         glColor3f(10, 0, 0);
         for(DistancePatch dp : world.getDistancePatched()){
+            glPushMatrix();
             double verts[][] = new double[8][3];
             int edge = dp.getEdgeLength();
-            verts[0][0] = dp.getPostion()[0]-edge/2;
-            verts[0][1] = dp.getPostion()[1]+edge/2;
-            verts[0][2] = dp.getPostion()[2]-edge/2;
+            verts[0][0] = -edge/2f;
+            verts[0][1] = +edge/2f;
+            verts[0][2] = -edge/2f;
             
-            verts[1][0] = dp.getPostion()[0]+edge/2;
-            verts[1][1] = dp.getPostion()[1]+edge/2;
-            verts[1][2] = dp.getPostion()[2]-edge/2;
+            verts[1][0] = +edge/2f;
+            verts[1][1] = +edge/2f;
+            verts[1][2] = -edge/2f;
             
-            verts[2][0] = dp.getPostion()[0]+edge/2;
-            verts[2][1] = dp.getPostion()[1]+edge/2;
-            verts[2][2] = dp.getPostion()[2]+edge/2;
+            verts[2][0] = +edge/2f;
+            verts[2][1] = +edge/2f;
+            verts[2][2] = +edge/2f;
             
-            verts[3][0] = dp.getPostion()[0]-edge/2;
-            verts[3][1] = dp.getPostion()[1]+edge/2;
-            verts[3][2] = dp.getPostion()[2]+edge/2;
+            verts[3][0] = -edge/2f;
+            verts[3][1] = +edge/2f;
+            verts[3][2] = +edge/2f;
             
-            verts[4][0] = dp.getPostion()[0]-edge/2;
-            verts[4][1] = dp.getPostion()[1]-edge/2;
-            verts[4][2] = dp.getPostion()[2]-edge/2;
+            verts[4][0] = -edge/2f;
+            verts[4][1] = -edge/2f;
+            verts[4][2] = -edge/2f;
             
-            verts[5][0] = dp.getPostion()[0]+edge/2;
-            verts[5][1] = dp.getPostion()[1]-edge/2;
-            verts[5][2] = dp.getPostion()[2]-edge/2;
+            verts[5][0] = +edge/2f;
+            verts[5][1] = -edge/2f;
+            verts[5][2] = -edge/2f;
             
-            verts[6][0] = dp.getPostion()[0]+edge/2;
-            verts[6][1] = dp.getPostion()[1]-edge/2;
-            verts[6][2] = dp.getPostion()[2]+edge/2;
+            verts[6][0] = +edge/2f;
+            verts[6][1] = -edge/2f;
+            verts[6][2] = +edge/2f;
             
-            verts[7][0] = dp.getPostion()[0]-edge/2;
-            verts[7][1] = dp.getPostion()[1]-edge/2;
-            verts[7][2] = dp.getPostion()[2]+edge/2;
+            verts[7][0] = -edge/2f;
+            verts[7][1] = -edge/2f;
+            verts[7][2] = +edge/2f;
+            
+            switch(dp.getLevel()){
+                case 0 :
+                    glColor3f(1, 0, 0);
+                    break;
+                case 1 :
+                    glColor3f(0, 1, 0);
+                    break;
+                case 2 :
+                    glColor3f(0, 0, 1);
+                    break;
+                case 3 :
+                    glColor3f(1, 0, 1);
+                    break;
+            }
+            
+            glTranslated(dp.getPostion()[0], dp.getPostion()[1], dp.getPostion()[2]);
             glBegin(GL_LINES);
                 glVertex3d(verts[0][0], verts[0][1], verts[0][2]); glVertex3d(verts[1][0], verts[1][1], verts[1][2]);
                 glVertex3d(verts[0][0], verts[0][1], verts[0][2]); glVertex3d(verts[3][0], verts[3][1], verts[3][2]);
@@ -110,6 +128,7 @@ public class GLDebugRenderer {
                 glVertex3d(verts[5][0], verts[5][1], verts[5][2]); glVertex3d(verts[4][0], verts[4][1], verts[4][2]);
                 glVertex3d(verts[5][0], verts[5][1], verts[5][2]); glVertex3d(verts[6][0], verts[6][1], verts[6][2]);
             glEnd();
+            glPopMatrix();
         }
     }
 }
