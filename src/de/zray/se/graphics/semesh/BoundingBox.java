efd229da765ad4a39db81760eaed451c589b113e
Vortex Acherontic <vortex@z-ray.de>
2018-10-11 22:06:55 +0200

    #55 improving BoundingBox

--- src/de/zray/se/graphics/semesh/BoundingBox.java
+++ src/de/zray/se/graphics/semesh/BoundingBox.java
@@ -6,83 +6,78 @@
 package de.zray.se.graphics.semesh;
 
 import de.zray.se.storages.AssetLibrary;
-import de.zray.se.utils.SEUtils;
 import de.zray.se.world.Actor;
 import de.zray.se.world.Entity;
 import javax.vecmath.Vector3d;
-import javax.vecmath.Vector3f;
 
 /**
  *
  * @author Vortex Acherontic
  */
 public class BoundingBox{
+    public static final int BOTTOM_BACK_LEFT = 0, BOTTOM_BACK_RIGHT = 1,
+            BOTTOM_FRONT_RIGHT = 2, BOTTOM_FRONT_LEFT = 3, TOP_BACK_LEFT = 4,
+            TOP_BACK_RIGHT = 5, TOP_FRONT_RIGHT = 6, TOP_FRONT_LEFT = 7;
     float radius = 0;
-    Vertex verts[] = new Vertex[8], defaultVerts[] = new Vertex[8];
+    
     Orientation ori;
+    Vertex vertecies[] = new Vertex[8];
+    
     
     public BoundingBox(Entity parent){
-        calculateBoindingBox(parent);
+        calculateBoundingBox(parent);
     }
     
-    private void calculateBoindingBox(Entity parent){
+    private void calculateBoundingBox(Entity parent){
         this.ori = parent.getOrientation();
         if(parent instanceof Actor){
+            for(int i = 0; i < 8; i++){
+                vertecies[i] = new Vertex(0, 0, 0);
+            }
             Actor parentActor = (Actor) parent;
             int mDataID =  parentActor.getRootMesh().getSEMeshData();
             MeshData mData = AssetLibrary.get().getMesh(mDataID);
-            float minX = 0, maxX = 0, minY = 0, maxY = 0, minZ = 0, maxZ = 0;
-            Vector3f center = new Vector3f(0, 0, 0);
-            for(Vertex v : mData.getVertecies()){
-                if(v.vX < minX){
-                    minX = v.vX;
+            if(mData.getVertecies().size() > 0){
+                Vertex vX0Y0Z0 = mData.getVertecies().get(0);
+                Vertex vX1Y0Z0 = mData.getVertecies().get(0);
+
+                Vertex vX0Y1Z0 = mData.getVertecies().get(0);
+                Vertex vX1Y1Z0 = mData.getVertecies().get(0);
+
+                Vertex vX0Y0Z1 = mData.getVertecies().get(0);
+                Vertex vX1Y0Z1 = mData.getVertecies().get(0);
+
+                Vertex vX0Y1Z1 = mData.getVertecies().get(0);
+                Vertex vX1Y1Z1 = mData.getVertecies().get(0);
+                for(Vertex v : mData.getVertecies()){
+                    if(v.vX < vX0Y0Z0.vX && v.vY < vX0Y0Z0.vY  && v.vZ < vX0Y0Z0.vZ){
+                        vX0Y0Z0 = v;
+                    } else if(v.vX > vX1Y0Z0.vX && v.vY < vX1Y0Z0.vY  && v.vZ < vX1Y0Z0.vZ){
+                        vX1Y0Z0 = v;
+                    } else if(v.vX < vX0Y1Z0.vX && v.vY > vX0Y1Z0.vY  && v.vZ < vX0Y1Z0.vZ){
+                        vX0Y1Z0 = v;
+                    } else if(v.vX > vX1Y1Z0.vX && v.vY > vX1Y1Z0.vY  && v.vZ < vX1Y1Z0.vZ){
+                        vX1Y1Z0 = v;
+                    } else if(v.vX < vX0Y0Z1.vX && v.vY < vX0Y0Z1.vY  && v.vZ > vX0Y0Z1.vZ){
+                        vX0Y0Z1 = v;
+                    } else if(v.vX > vX1Y0Z1.vX && v.vY < vX1Y0Z1.vY  && v.vZ > vX1Y0Z1.vZ){
+                        vX1Y0Z1 = v;
+                    } else if(v.vX < vX0Y1Z1.vX && v.vY > vX0Y1Z1.vY  && v.vZ > vX0Y1Z1.vZ){
+                        vX0Y1Z1 = v;
+                    } else if(v.vX > vX1Y1Z1.vX && v.vY > vX1Y1Z1.vY  && v.vZ > vX1Y1Z1.vZ){
+                        vX1Y1Z1 = v;
+                    }
                 }
-                if(v.vX > maxX){
-                    maxX = v.vX;
-                }
-                if(v.vY < minY){
-                    minY = v.vY;
-                }
-                if(v.vY > maxY){
-                    maxY = v.vY;
-                }
-                if(v.vZ < minZ){
-                    minZ = v.vZ;
-                }
-                if(v.vZ > maxZ){
-                    maxZ = v.vZ;
-                }
-                Vector3f vertex = new Vector3f(v.vX, v.vY, v.vZ);
-                Vector3f path = SEUtils.getVector(center, vertex);
-                float rad = SEUtils.getLenght(path);
-                if(rad > radius){
-                    radius = rad;
-                }
+                vertecies[0] = vX0Y0Z0;
+                vertecies[1] = vX1Y0Z0;
+                vertecies[2] = vX0Y1Z0;
+                vertecies[3] = vX1Y1Z0;
+                vertecies[4] = vX0Y0Z1;
+                vertecies[5] = vX1Y0Z1;
+                vertecies[6] = vX0Y1Z1;
+                vertecies[7] = vX1Y1Z1;
             }
-            verts[0] = new Vertex(minX, minY, minZ);
-            verts[1] = new Vertex(maxX, minY, minZ);
-            verts[2] = new Vertex(minX, maxY, minZ);
-            verts[3] = new Vertex(maxX, maxY, minZ);
-            verts[4] = new Vertex(minX, minY, maxZ);
-            verts[5] = new Vertex(maxX, minY, maxZ);
-            verts[6] = new Vertex(minX, maxY, maxZ);
-            verts[7] = new Vertex(maxX, maxY, maxZ);
             
-            defaultVerts[0] = new Vertex(minX, minY, minZ);
-            defaultVerts[1] = new Vertex(maxX, minY, minZ);
-            defaultVerts[2] = new Vertex(minX, maxY, minZ);
-            defaultVerts[3] = new Vertex(maxX, maxY, minZ);
-            defaultVerts[4] = new Vertex(minX, minY, maxZ);
-            defaultVerts[5] = new Vertex(maxX, minY, maxZ);
-            defaultVerts[6] = new Vertex(minX, maxY, maxZ);
-            defaultVerts[7] = new Vertex(maxX, maxY, maxZ);
-            /*System.out.println("[BoundingBox]: Builded BBox");
-            System.out.println("=> radius: "+radius);
-            System.out.println("=> Vertecies:");
-            for(Vertex v : verts){
-            System.out.println("=> => "+v.vX+" "+v.vY+" "+v.vZ);
-            System.out.println("=> => <= <= ");
-            }*/
         }
     }
     
@@ -95,25 +90,12 @@ public class BoundingBox{
         return false;
     }
   
-    public boolean inside(Vector3d point, Orientation vecOri){
-        return realInside(point, vecOri);
-    }
-    
-    public boolean inside(Vector3d point){
-        return realInside(point, new Orientation(null));
-    }
-    
-    private boolean realInside(Vector3d point, Orientation vecOri){
-        /*if(point.x+vecOri.getPositionVec().x >= xmin+ori.getPositionVec().x && point.x < xmax+ori.getPositionVec().x){
-        return true;
-        }
-        if(point.y+vecOri.getPositionVec().y >= ymin+ori.getPositionVec().y && point.y < ymax+ori.getPositionVec().y+vecOri.getPositionVec().y){
-        return true;
-        }
-        if(point.z+vecOri.getPositionVec().z >= zmin+ori.getPositionVec().z && point.z < zmax+ori.getPositionVec().z+vecOri.getPositionVec().z){
-        return true;
-        }*/
+    public boolean isInside(Vector3d point){
         return false;
+    }
+    
+    private boolean isBetween(double start, double end, double val){
+        return val <= end && val >= start;
     }
     
     /**
@@ -123,21 +105,15 @@ public class BoundingBox{
     public double getRadius(){
         return radius;
     }
-    
-    public Vertex[] getVertecies(){
-        return verts;
-    }
-    
+       
     public Orientation getOrientation(){
         return ori;
     }
     
     public void reCalc(){
-        double scale[] = ori.getScale();
-        for(int i = 0; i < verts.length; i++){
-            Vector3f vertex = new Vector3f(defaultVerts[i].vX, defaultVerts[i].vY, defaultVerts[i].vZ);
-            Vector3f scaleVec = new Vector3f((float) scale[0], (float) scale[1], (float) scale[2]);
-            vertex.scale(radius, scaleVec);
-        }
+    }
+    
+    public Vertex[] getBoundingVertecies(){
+        return vertecies;
     }
 }
